c7826af4bd85705f1957772ef9fa5ee598354e7e
Grant J. Butler <grantjbutler@users.noreply.github.com>
2025-12-24 17:17:03 -0500

    Attempt some animation.

--- server/src/atem/upsteam_key.ts
+++ server/src/atem/upsteam_key.ts
@@ -1,10 +1,16 @@
 import { Atem } from 'atem-connection';
 import { Actions } from '../action.js';
+import * as TWEEN from '@tweenjs/tween.js'
+
+const DURATION = 500;
 
 class UpstreamKey {
   atem: Atem;
   meIndex: number;
   keyerIndex: number;
+
+  animation: TWEEN.Tween<{x: number, y: number}> | undefined;
+  interval: NodeJS.Timeout | undefined;
 
   constructor(atem: Atem, meIndex: number, keyerIndex: number) {
     this.atem = atem;
@@ -13,10 +19,7 @@ class UpstreamKey {
   }
 
   scale({ scale }: { scale: number }): void {
-    this.atem.setUpstreamKeyerDVESettings({
-      sizeX: scale * 1000,
-      sizeY: scale * 1000
-    }, this.meIndex, this.keyerIndex)
+    this._scaleTo({ x: scale * 1000, y: scale * 1000 }, DURATION);
   }
 
   shrink({ magnitude }: { magnitude: number }): void {
@@ -24,17 +27,57 @@ class UpstreamKey {
     if (!existingSize) return;
 
     let targetSize = existingSize * magnitude;
-    this.atem.setUpstreamKeyerDVESettings({
-      sizeX: targetSize,
-      sizeY: targetSize
-    }, this.meIndex, this.keyerIndex);
+
+    this._scaleTo({ x: targetSize, y: targetSize }, DURATION);
   }
 
   reset(): void {
+    this._scaleTo({ x: 1000, y: 1000 }, DURATION);
+  }
+
+  _scaleTo(size: { x: number, y: number }, duration: number): void {
+    this.animation?.stop();
+    clearInterval(this.interval);
+    
+    if (duration <= 0) {
+      this._jumpTo(size);
+      return;
+    }
+
+    let existingSettings = this.atem.state?.video.mixEffects[this.meIndex]?.upstreamKeyers[this.keyerIndex]?.dveSettings;
+    if (!existingSettings) {
+      this._jumpTo(size);
+      return;
+    }
+
+    this.animation = new TWEEN.Tween<{x: number, y: number}>({
+      x: existingSettings.sizeX,
+      y: existingSettings.sizeY
+    })
+    .to(size, duration)
+    .onUpdate((size) => {
+      this._jumpTo(size);
+    })
+    .onComplete(() => {
+      this.animation?.stop();
+      this.animation = undefined;
+      clearInterval(this.interval);
+    })
+    .start();
+
+    this.interval = setInterval(
+      () => {
+        this.animation?.update();
+      },
+      33
+    );
+  }
+
+  _jumpTo(size: { x: number, y: number }): void {
     this.atem.setUpstreamKeyerDVESettings({
-      sizeX: 1000,
-      sizeY: 1000
-    }, this.meIndex, this.keyerIndex);
+      sizeX: size.x,
+      sizeY: size.y
+    }, this.meIndex, this.keyerIndex)
   }
 
   getActions(): Actions {
